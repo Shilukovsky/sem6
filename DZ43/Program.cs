327adf1b0067ca551dd4a30660e1e1a23850c600
Nikolay <cccp_ussr@bk.ru>
2022-08-14 15:36:22 +0300

    added DZ43

---
+++ DZ43/Program.cs
@@ -0,0 +1,18 @@
+﻿int GetUserInput()
+{
+    Console.WriteLine("введите число:");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int k1 = GetUserInput();
+int k2 = GetUserInput();
+int b1 = GetUserInput();
+int b2 = GetUserInput();
+
+double x = -(b1 - b2) / (k1 - k2);
+double y = k2 * x + b2;
+
+x = Math.Round(x, 1);
+y = Math.Round(y, 1);
+
+Console.WriteLine($"Пересечение в точке: ({x}; {y})");
